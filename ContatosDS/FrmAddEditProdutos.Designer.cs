a2a7a66ac8d32e7fb91afc57dce4fefb2605d70a
Gabriel Sugahara <56739302+TekoKnife@users.noreply.github.com>
2020-07-09 20:28:41 -0300

    Add files via upload

---
+++ ContatosDS/FrmAddEditProdutos.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace ContatosDS
+{
+    partial class FrmAddEditProdutos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkGreen;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(670, 85);
+            this.panel1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.DarkGreen;
+            this.label1.Font = new System.Drawing.Font("Arial", 24F);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(31, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(505, 35);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "(title)";
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.Tomato;
+            this.label7.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(0, 77);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(670, 38);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "Produto";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label7.Visible = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.textBox2);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Location = new System.Drawing.Point(77, 118);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(518, 516);
+            this.panel2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.BackColor = System.Drawing.Color.Tomato;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(125, 371);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 28);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Deletar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.BackColor = System.Drawing.Color.DarkGreen;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(15, 371);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 28);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Salvar";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(10, 208);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 29);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Quantidade";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Comic Sans MS", 12F);
+            this.textBox2.Location = new System.Drawing.Point(15, 240);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(484, 30);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(10, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Produto";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(15, 102);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(484, 30);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2000;
+            // 
+            // FrmAddEditProdutos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(670, 548);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.panel1);
+            this.Name = "FrmAddEditProdutos";
+            this.Text = "FrmAddEditProdutos";
+            this.Load += new System.EventHandler(this.FrmAddEditProdutos_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ ContatosDS/FrmAddEditProdutos.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TooSharp.Models;
+using System.Text.RegularExpressions;
+
+namespace ContatosDS
+{
+    public partial class FrmAddEditProdutos : Form
+    {
+        Almoxarifado _Almoxarifado = null;
+        public FrmAddEditProdutos()
+        {
+            InitializeComponent();
+            label1.Text = "Add Produto";
+        }
+
+        public FrmAddEditProdutos(Almoxarifado almoxarifado)
+        {
+            InitializeComponent();
+            button1.Text = "Salvar";
+            button2.Visible = true;
+            _Almoxarifado = almoxarifado;
+            label1.Text = "Editar Produto";
+            //Validação
+            _Almoxarifado.onValidationError += Almoxarifado_onValidationError;
+            textBox1.Text = _Almoxarifado.Produto;
+            textBox2.Text = _Almoxarifado.Quantidade;
+            
+        }
+        void ShowError(string Text)
+        {
+            label7.Text = Text;
+            panel2.Visible = true;
+            timer1.Start();
+        }
+
+        private void tmrError_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            panel2.Visible = false;
+        }
+
+
+        private void FrmAddEditProdutos_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (_Almoxarifado == null)
+            {
+                //new contact
+                Almoxarifado almoxarifado = new Almoxarifado()
+                {
+                    Produto = textBox1.Text,
+                    Quantidade = textBox2.Text
+                };
+
+                //Validation
+                almoxarifado.onValidationError += Almoxarifado_onValidationError;
+
+                if (textBox1.Text == "")
+                {
+                    MessageBox.Show("Preencher o campo Produto");
+                    return;
+                }
+                if (textBox2.Text == "")
+                {
+                    MessageBox.Show("Preencher o campo Quantidade");
+                    return;
+                }
+                
+
+
+
+                //save
+                if (almoxarifado.Save() > 0) MessageBox.Show("Produto Salvo");
+                {
+                    
+                    this.Close();
+                }
+            }
+            else
+            {
+                //update contact
+                _Almoxarifado.Produto = textBox1.Text;
+                _Almoxarifado.Quantidade = textBox2.Text;
+                
+                //save
+                if (_Almoxarifado.Save() > 0) MessageBox.Show("Produto Atualizado");
+                {
+                    MessageBox.Show("Produto Atualizado");
+                    this.Close();
+                }
+            }
+        }
+        private void Almoxarifado_onValidationError(object sender, TooSharp.Core.TsExeptionArgs e)
+        {
+            ShowError(e.exception.Message);
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !(char.IsLetter(e.KeyChar) || e.KeyChar == (char)Keys.Back);
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !(char.IsDigit(e.KeyChar) || e.KeyChar == (char)Keys.Back);
+        }
+    }
+}
